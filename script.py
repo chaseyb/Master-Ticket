90669fae23888f95a10d34fb61e0e1473f79bd7e
Chase Johnson <Chase.b.johnson@gmail.com>
2020-11-03 15:15:34 -0800

    started task 2.

--- script.py
+++ script.py
@@ -3,4 +3,7 @@ TICKET_PRICE = 10
 tickets_remaining = 100
 
 # Shows the user how many tickets are remaining 
-print("There are {} tickets remaining.".format(tickets_remaining)
+print("There are {} tickets remaining.".format(tickets_remaining))
+
+# Gathers the user's name and assign it to a new variable 
+name = input("What is your name? ")
